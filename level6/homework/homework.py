fb925738ad893169ddd10f44d6ad1e1a414c6fc3
bendoza77 <gabrieljobava123@gmail.com>
2024-06-02 15:37:10 +0400

    commit

--- level6/homework/homework.py
+++ level6/homework/homework.py
@@ -1,13 +1,18 @@
 #Task 2
 
 num1 = int(input("Enter the first number: "))
+#Call the variable that you assigned the value to
 num2 =int( input("Enter the second number: "))
+#Call the variable that you assigned the value to
 num3 = int(input("Enter the third number: "))
+#Call the variable that you assigned the value to
 
 print(num1 + num2 + num3)
 print(num1 - num2 - num3)
 print(num1 * num2 * num3)
 print(num1/num2/num3)
+
+#Then I used the print command to display all the math operations on the screen
 
 
 
@@ -15,19 +20,34 @@ print(num1/num2/num3)
 
 
 name1 = str(input("Enter your name: "))
+##Call the variable that you assigned the value to
 female1=  str(input("Enter your female: "))
+##Call the variable that you assigned the value to
 age1 = int(input("Enter your age: "))
+##Call the variable that you assigned the value to
 password1 = input("Enter your password: ")
+##Call the variable that you assigned the value to
 
 print(name1)
 print(female1)
 print(age1)
 print(password1)
 
+#I used the print command to display it on the screen
+#everything
+#What I write in the input variable
+
 
 #Task 4
 
-name2 = str(input("Enter your name: "))
-age2 = int(input("Enter your age: "))
+my_name2 = str(input("Enter your name: "))
+##Call the variable that you assigned the value to
+#One of the best practices snake_case is used here, which makes it easier to read a sentence that contains many words
+my_age2 = int(input("Enter your age: "))
+##Call the variable that you assigned the value to
+#One of the best practices snake_case is used here, which makes it easier to read a sentence that contains many words
 
-print(f"hello nice to meet you me name is {name2} and i am {age2} years old")
+print(f"hello nice to meet you me name is {my_name2} and i am {my_age2} years old")
+
+#Here I've used the print command to print everything on the screen that I put in the input variable, 
+#but I've also used the f string which helps me create sentences without any extra +
