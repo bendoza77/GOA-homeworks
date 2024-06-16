1d1571291e6e2590834b0f86c69dab2ac6502697
bendoza77 <gabrieljobava123@gmail.com>
2024-06-16 17:19:12 +0400

    commit

--- level 10/homework/homework.py
+++ level 10/homework/homework.py
@@ -1,1 +1,107 @@
-print(25 >= 10 or 10!=10) #True
+# Task 2
+
+for i in range (1, 101, 2 ):
+    print(i)
+
+
+# Task 3
+
+num1 = 0
+
+while num1 < 10:
+    print(num1)
+    num1 += 1
+
+# Task 4
+
+num1 = 1
+sum1 = 0
+
+while sum1 < 10:
+    sum1 += num1
+    num1 += 1
+    print(sum1)
+
+
+# Task 5
+
+    #1)
+for i in range(100 , 1 , -2):
+    print(i)
+
+
+    #2)
+
+for i in range(50 , 10 , -5):
+    print(i)
+
+
+
+    #3)
+
+
+for i in range(int(input("Enter you number: "))):
+    print("hello world")
+
+
+
+    #4)
+
+for i in range(5):
+    print(str(input("Enter word: ")))
+
+
+    #5)
+
+for i in range(5):
+    print("hello world")
+
+
+    #while loop
+
+    #1)
+
+num1 = 1
+
+while num1 < 10:
+    print(num1)
+    num1 += 1
+
+    #2)
+
+num1 = 0
+sum1 = 1
+
+while sum1 <= 40:
+    sum1 += num1
+    num1 += 1
+    print(sum1)
+
+
+    #3)
+
+num1 = 0
+
+while num1 < 10:
+    print(num1)
+    num1 += 5
+
+
+    #4)
+
+secret = 123
+password = secret
+
+print(int(input("Enter you password: ")))
+if password == secret:
+    print("correct")
+else: password != secret
+print("try again")
+
+    #5)
+
+num1 = 10
+
+while num1 < 20:
+    print("Hello")
+    num1 += 2
