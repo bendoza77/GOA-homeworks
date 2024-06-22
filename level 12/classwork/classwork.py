11caf14d3e7e3ad13b4822b7863ab6032d0e9a1c
bendoza77 <gabrieljobava123@gmail.com>
2024-06-22 20:23:38 +0400

    commit

--- level 12/classwork/classwork.py
+++ level 12/classwork/classwork.py
@@ -1,7 +1,21 @@
-num1 = int(input("Enter you firts number: "))
-num2 = int(input("Enter your second number: "))
+# Task 1
 
-print(num1 + num2)
-print(num1 - num2)
-print(num1 * num2)
-print(num1 / num2)
+age = int(input("How old are you: "))
+
+if age < 13:
+    print("you are kid")
+elif age > 13 and age < 20:
+    print("you are teenager")
+else:
+    print("you are grown")
+
+    # Task 2
+
+num1 = 1
+
+while num1 < 100:
+        if  num1 == 55:
+            pass
+        else:
+            print(num1)
+            num1 += 1
---
+++ level 12/homework/homework.py
@@ -0,0 +1,110 @@
+#  Task 2
+
+
+secret_number = 4
+
+guess = int(input("guess your secret_number: "))
+
+if guess == 4:
+    print("its correct")
+else:
+    print("its incorrect")
+
+
+# Task 3
+
+sum1 = 0
+
+for i in range(1, 100):
+    sum1 += i
+    print(sum1)
+
+
+
+# Task 4
+
+sum1 = 0
+
+for i in range (1,1000):
+    if i ==  (500,600):
+        pass
+else:
+    print(sum1)
+
+
+
+
+# Task 6
+
+num1 = 1
+sum1 = 0
+
+while sum1 < 10:
+    sum1 * num1
+    num1 += 1
+    print(num1)
+
+
+
+
+
+
+
+
+# Task 7
+
+num1  = int(input("Enter your number: "))
+
+if num1 % 2 == 0:
+    print("luwia")
+else: 
+    print("kentia")
+
+
+
+
+
+
+
+
+
+
+
+
+#  Task 8
+
+
+grade = int(input("Enter your grade: "))
+
+
+if grade >= 90 and grade <= 100:
+    print("Grade A")
+elif grade >= 80 and grade <= 89:
+    print("Grade B")
+elif grade >=  70 and grade <= 79:
+    print("Grade C")
+elif grade >= 60 and grade <= 69:
+    print("Grade E")
+else:
+    print("Grade F")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
