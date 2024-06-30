2b2b3e66b49d7a7458647df3ebc5950e67c86bb6
bendoza77 <gabrieljobava123@gmail.com>
2024-06-30 09:49:04 +0400

    commit

---
+++ level 14/classwork/classwork.py
@@ -0,0 +1,56 @@
+# Task 1
+
+
+programming_languages = ["phyton", "javascript" , "css", "html", "java"]
+
+print(programming_languages[0])
+print(programming_languages[1])
+print(programming_languages[2])
+print(programming_languages[3])
+print(programming_languages[4])
+
+
+# Task 2
+
+programming_languages = ["phyton", "javascript" , "css", "html", "java"]
+
+
+programming_languages[1] = "java"
+programming_languages[4] = "phyton"
+
+print(programming_languages[1])
+print(programming_languages[4])
+
+# Task 3
+
+programming_languages = ["phyton", "javascript" , "css", "html", "java"]
+
+
+print(programming_languages[-1])
+print(programming_languages[-2])
+print(programming_languages[-3])
+print(programming_languages[-4])
+print(programming_languages[-5])
+
+programming_languages = ["phyton", "javascript" , "css", "html", "java"]
+
+
+programming_languages[-1] = "html"
+programming_languages[-4] = "c++"
+
+print(programming_languages[-1])
+print(programming_languages[-4])
+
+
+list1 = [1,2,3,4,5,5,6,7,8,9,9,10]
+
+x = int(input("slice1: "))
+y = int(input("slice2: "))
+
+
+print(list1[x:-y])
+
+
+
+
+
---
+++ level 14/homework/homework.py
@@ -0,0 +1,74 @@
+# Task 2
+
+list1 = [2, 51, 11, 13, 51, 100]
+
+    # 1
+
+print(list1[0])
+print(list1[1])
+print(list1[2])
+print(list1[3])
+print(list1[4])
+print(list1[5])
+
+    # 2
+
+print(list1[-1])
+print(list1[-2])
+print(list1[-3])
+print(list1[-4])
+print(list1[-5])
+print(list1[-6])
+
+
+    # 3
+
+list1[5] = 99
+
+print(list1[0])
+print(list1[1])
+print(list1[2])
+print(list1[3])
+print(list1[4])
+print(list1[5])
+
+
+    # 4
+
+list1[4] = 50
+
+
+print(list1[0])
+print(list1[1])
+print(list1[2])
+print(list1[3])
+print(list1[4])
+print(list1[5])
+
+    # 5
+
+
+list1 = [2, 51, 11, 13, 51, 100]
+
+print(list1[3:6])
+
+
+    # 6
+
+print(list1[0:3])
+
+
+    # 7
+
+print(list1[2:4])
+
+    # 8
+
+list1 = [2, 51, 11, 13, 51, 100]
+
+
+x = int(input("slice1: "))
+y = int(input("slice2: "))
+
+
+print(list1[-x:y])
