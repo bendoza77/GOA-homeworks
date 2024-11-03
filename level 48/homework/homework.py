d2908806dc99e5932fd5cb9cb0a546f1caa09b14
bendoza77 <gabrieljobava123@gmail.com>
2024-11-03 12:34:51 +0400

    commit

---
+++ level 48/homework/homework.py
@@ -0,0 +1,15 @@
+# Task 2
+
+# def order_weight(s):
+#     # your code
+    
+#     res = [(sum(int(j) for j in i), i) for i in s.split()]
+    
+#     sorted_res = sorted(res)
+    
+#     return " ".join(i[1] for i in sorted_res)
+
+
+
+# მე აქ შევქმენი ცვალდი res რომელსაც გავუწერე list comarasiion სადაც არის შექმნილი tupler რომელშიც არის რიცხვის თითოეული ელემენტის ჯამი და თვით რიცხვი
+# შემდეგ დავსორტე და პირველი ელემენტი ანუ ამ რიცხვების ჯამი მოვაშორე და მარტო რიცხვი დავტოვე.
