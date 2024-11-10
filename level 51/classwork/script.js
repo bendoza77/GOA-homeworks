610aa7582d5cd8281ea88ba6984cdcb47a1bfc49
bendoza77 <gabrieljobava123@gmail.com>
2024-11-10 15:21:55 +0400

    commit

---
+++ level 51/classwork/script.js
@@ -0,0 +1,34 @@
+    // Task 1
+
+// javascript is one of the popular programming languages ​​used for both backend and frontend. 
+// javascript helps us make the site comfortable and functional. Using javascript, it is possible to 
+// write such programs for the site as shopping, creating a 
+// shopping cart for the sales site, games, calculators for mobile apps, and other programs.
+
+    // Task 2
+
+// An event is an event that happens on the computer, for example, clicking the mouse, going from one website to another, 
+// clicking a link, pressing a button, and everything that happens on the computer screen.
+
+
+    // Task 3
+
+// In javascript, we create variables dynamically, and we see this by the value assigned to the variable. Statistically, 
+// creating a variable is when we write what type of data it should be before creating it, and dynamically, 
+// it is reduced when the computer knows which data type it is by the sign of the variable.
+
+
+    // Task 4
+
+// We create variables in javascript with var, let and const keywords
+
+    // Task 5
+
+// let num = 5;
+// let num = 6;
+
+// This code throws an error because two identical variable names in javascript are an error
+
+    // Task 6
+
+// camelCase_ is named so because the design of this variable looks like a camel's hump
---
+++ level 51/homework/script.js
@@ -0,0 +1,22 @@
+    // Task 2
+
+// The document object model is how textContent or getElementByiD helps us manage all HTML elements 
+// associated with javascript or change their values ​​using this function.
+
+    // Task 3
+
+let firstNum = 10
+let secondNum = 20
+
+console.log(firstNum + secondNum)
+
+
+    // Task 4
+
+// A console is a JavaScript object used to display various 
+// types of information, mainly for debugging and logging purposes to the developer.
+
+
+    // Task 5
+
+// let keyword helps us to create a variable that we want to change without it we literally cannot create a variable in javascript
