951d99e4a20b68a45d473bf95bdbf97afe34457f
bendoza77 <gabrieljobava123@gmail.com>
2025-06-22 14:01:16 +0400

    commit

---
+++ level 113/classwork/src/components/MovieCard.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+let change = false;
+
+const MovieCard = (props) => {
+
+    const [smallDec, setDec] = useState("The small description: " + props.description);
+
+    const handleFullDec = () => {
+        change = true;
+        setDec("The full Description: " + props.fullDec);
+    }
+
+    const handleSmallDec = () => {
+        change = false;
+        setDec("The small description: " + props.description);
+    }
+
+    return (
+        <>
+            <div>
+                <h1>{props.title}</h1>
+                <img src={props.image} alt="" />
+                <p>{smallDec}</p>
+                <button onClick={!change ? () => handleFullDec() : () => handleSmallDec()}>{change ? "Show small description" : "Show full description"}</button>
+            </div>
+        </>
+    );
+
+}
+
+
+export default MovieCard;
